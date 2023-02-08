e32081c1c3abf53041335cef726d322d4fc59fe5
Jay Faust <jay.faust3@gmail.com>
2023-02-07 22:30:08 -0500

    Updated PermissionCRUDWriteRequest

--- src/main/kotlin/com/common/models/dataaccess/permission/PermissionPartitionKeyBuilder.kt
+++ src/main/kotlin/com/common/models/dataaccess/permission/PermissionPartitionKeyBuilder.kt
@@ -3,5 +3,5 @@ package com.permissionapi.common.models.dataaccess
 import com.permissionapi.common.enums.ActorType
 
 data class PermissionPartitionKeyBuilder (private val actorType: ActorType, private val entityId: String) {
-    val primaryKey: String = "$actorType::$entityId"
+    val partitionKey: String = "$actorType::$entityId"
 }
