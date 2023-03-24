e3b123d4ebbb832f4ec2a8a7c2edd6d041b5b217
Jay Faust <jay.faust3@gmail.com>
2023-03-23 23:00:53 -0400

    Package/jvm version updates

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM gradle:7.5.1-jdk11-alpine as builder
 COPY build.gradle.kts .
 COPY src ./src
 RUN gradle clean build --no-daemon
-FROM openjdk:11-jre-slim
+FROM openjdk:17-jdk-slim
 EXPOSE 80
 COPY --from=builder /home/gradle/build/libs/*.jar /app.jar
 CMD [ "java", "-jar", "-Djava.security.egd=file:/dev/./urandom", "/app.jar" ]
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,17 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.3.3.RELEASE"
-    id("io.spring.dependency-management") version "1.0.10.RELEASE"
-    kotlin("jvm") version "1.6.0"
-    kotlin("plugin.spring") version "1.3.72"
-    kotlin("plugin.jpa") version "1.3.72"
-    kotlin("plugin.serialization") version "1.6.0"
+    id("org.springframework.boot") version "3.0.5"
+    id("io.spring.dependency-management") version "1.1.0"
+    kotlin("jvm") version "1.8.0"
+    kotlin("plugin.spring") version "1.8.10"
+    kotlin("plugin.jpa") version "1.8.10"
+    kotlin("plugin.serialization") version "1.8.10"
 }
 
 group = "com.permission.api"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_11
+java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
     mavenCentral()
@@ -20,32 +20,32 @@ repositories {
 dependencies {
     // kotlin
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.8.10")
 
     // spring
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-security")
-    implementation("org.springframework.vault:spring-vault-dependencies:2.3.2")
-    implementation("org.springframework.vault:spring-vault-core:2.3.2")
-    implementation("org.springframework.data:spring-data-commons:2.3.3.RELEASE")
+    implementation("org.springframework.boot:spring-boot-starter-web:3.0.4")
+    implementation("org.springframework.boot:spring-boot-starter-security:3.0.4")
+    implementation("org.springframework.vault:spring-vault-dependencies:3.0.0")
+    implementation("org.springframework.vault:spring-vault-core:3.0.0")
+    implementation("org.springframework.data:spring-data-commons:3.0.3")
 
     // kotlinx
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
 
     //aws
-    implementation("com.amazonaws:aws-java-sdk:1.12.403")
+    implementation("com.amazonaws:aws-java-sdk:1.12.429")
 
     // rabbit-mq
-    implementation("org.springframework:spring-messaging:5.3.25")
-    implementation("org.springframework.amqp:spring-rabbit:2.4.10")
+    implementation("org.springframework:spring-messaging:6.0.6")
+    implementation("org.springframework.amqp:spring-rabbit:3.0.2")
     implementation("org.springframework.boot:spring-boot-starter:2.7.9")
-    implementation("org.springframework.boot:spring-boot-starter-amqp:2.7.9")
+    implementation("org.springframework.boot:spring-boot-starter-amqp:3.0.4")
 
     // arrow
-    implementation("io.arrow-kt:arrow-core-jvm:1.1.2")
+    implementation("io.arrow-kt:arrow-core-jvm:1.1.5")
 
     // jwt / auth0
-    implementation("com.auth0:auth0-spring-security-api:1.5.1")
+    implementation("com.auth0:auth0-spring-security-api:1.5.2")
     implementation("io.jsonwebtoken:jjwt-extensions:0.11.5")
 
     // test
@@ -61,6 +61,6 @@ tasks.withType<Test> {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
 }
